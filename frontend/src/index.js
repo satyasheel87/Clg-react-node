9e30d924829730c4c2fea160b9940533d187a48a
satyasheel gatuam <satyasheelgautam87@gmial.com>
2026-04-18 10:14:24 +0530

    Add React Router future flags

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
+
+/*import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
@@ -8,4 +31,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);*/
